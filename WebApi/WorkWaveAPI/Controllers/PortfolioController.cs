0ad4eead85272806e7eb0a7b5752771fe187d779
Olaxander <mittsykh@ukr.net>
2023-03-04 21:20:14 +0200

    Create portfolio controller

--- WebApi/WorkWaveAPI/Controllers/PortfolioController.cs
+++ WebApi/WorkWaveAPI/Controllers/PortfolioController.cs
@@ -1,10 +1,14 @@
 ﻿using DAL.Extensions;
 using DAL.Models;
+using DAL.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WorkWaveAPI.ApiRequestModels;
+using System.IO;
+using Microsoft.AspNetCore.Hosting.Server;
+using WorkWaveAPI.Managers;
 
 namespace WorkWaveAPI.Controllers
 {
@@ -14,26 +18,78 @@ namespace WorkWaveAPI.Controllers
     public class PortfolioController : Controller
     {
         UserManager<User> _userManager;
-        public PortfolioController(UserManager<User>userManager) {
+        IRepository<PortfolioProject> _portfolioProjectRepository;
+        ProjectCategoryRepository _projectCategory;
+        public PortfolioController(UserManager<User>userManager,
+            IRepository<PortfolioProject>portfolio,
+            ProjectCategoryRepository projectCategory) 
+        {
             _userManager= userManager;
+            _portfolioProjectRepository= portfolio;
+            _projectCategory= projectCategory;
         }
 
         [HttpGet("/myPortfolio")]
-        public async Task<IActionResult> GetMyPortfolio()
+        public async Task<IEnumerable<PortfolioProject>> GetMyPortfolio()
         {
             User currentUser = await _userManager.GetUserByClaimsIdentityNameAsync(User.Identity);
-
-
-            return View();
+            return currentUser.PortfolioProjects;
+        }
+        [HttpDelete("/remove/{portfolioProjectId}")]
+        public async Task<IActionResult> DeleteProject(int portfolioProjectId)
+        {
+            if (portfolioProjectId != 0) 
+            {
+                PortfolioProject portfolioProject = _portfolioProjectRepository.GetById(portfolioProjectId);
+                if (portfolioProject != null) 
+                {
+                    await _portfolioProjectRepository.RemoveAsync(portfolioProject);
+                    return Ok(portfolioProject);
+                }
+            }
+            return BadRequest();
         }
 
+        [HttpPut("/update/{portfolioProjectId}")]
+        public async Task<IActionResult> UpdateProject(int portfolioProjectId)
+        {
+            if (portfolioProjectId != 0)
+            {
+                PortfolioProject portfolioProject = _portfolioProjectRepository.GetById(portfolioProjectId);
+                if (portfolioProject != null)
+                {
+                    _portfolioProjectRepository.UpdateAsync(portfolioProject);
+                    return Ok(portfolioProject);
+                }
+            }
+            return BadRequest();
+        }
+
+
         [HttpPost("/newPortfolioProject")]
-        public async Task<IActionResult> AddNewPortfolioProject([FromBody]AddPortfolioProjectModel model)
+        public async Task<IActionResult> AddNewPortfolioProject([FromBody]AddPortfolioProjectModel model, [FromServices]IWebHostEnvironment _host)
         {
             if (ModelState.IsValid)
             {
+                User currentUser = await _userManager.GetUserByClaimsIdentityNameAsync(User.Identity);
+                int id = _projectCategory.GetCategoryIdByName(model.ProjectCategoryName);
 
+                string uploads = Path.Combine(_host.WebRootPath, "uploads");
+                string filePath= await FileManager.CopyToAsync(model.PhotoFile, uploads);
+                PortfolioProject portfolio = new PortfolioProject 
+                { 
+                    Title= model.Title,
+                    ProjectCategoryId=id,
+                    Description=model.Description,
+                    OwnerId=currentUser.Id,
+                    PhotoPath=filePath,
+                    PhotoBase64=Base64Encoder.GetBase64String(model.PhotoFile),
+                    Url=model.ProjectUrl
+                };
 
+                _portfolioProjectRepository.Add(portfolio);
+
+                return Ok(portfolio);
             }
             return BadRequest();
         }
