1d9bd5e2505f0f1e99acf43e9f42fe9b683523eb
Мирослав <101458707+mirik297@users.noreply.github.com>
2023-03-04 21:43:28 +0200

    CategoryController realization

--- WebApi/WorkWaveAPI/Controllers/ProjectCategoriesController.cs
+++ WebApi/WorkWaveAPI/Controllers/ProjectCategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAL.Models;
+using DAL.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WorkWaveAPI.Controllers
 {
@@ -6,6 +9,45 @@ namespace WorkWaveAPI.Controllers
     [Route("api/v2/[controller]")]
     public class ProjectCategoriesController : Controller
     {
-     
+        IRepository<ProjectCategory> _repository;
+        public ProjectCategoriesController(IRepository<ProjectCategory> repository) {
+            _repository= repository;
+        }
+
+        [HttpGet]
+        [Route("/getallcategories")]
+        public ActionResult GetAllCategories()
+        {
+            var categories = _repository.GetAll();
+
+            if(categories == null) 
+                return NotFound();
+            else
+                return Json(categories);
+        }
+
+        [HttpGet]
+        [Route("/getcategorybyid")]
+        public async Task<ActionResult> GetCategoryById(int id)
+        {
+            var category = await _repository.GetByIdAsync(id);
+
+            if (category == null)
+                return NotFound();
+            else
+                return Json(category);
+        }
+
+        [HttpGet]
+        [Route("/getcategorybyname")]
+        public async Task<ActionResult> GetCategoryByName(string name)
+        {
+            var category = await _repository.GetAll().FirstOrDefaultAsync(c => c.Name == name);
+
+            if (category == null)
+                return NotFound();
+            else
+                return Json(category);
+        }
     }
 }
