11899a56801b0876a7b8e4431f9c2854844f236e
Olaxander <mittsykh@ukr.net>
2023-03-04 22:21:56 +0200

    Create file and base64 managers and members controller

--- WebApi/WorkWaveAPI/Controllers/MembersController.cs
+++ WebApi/WorkWaveAPI/Controllers/MembersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAL.Models;
+using DAL.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkWaveAPI.ApiRequestModels;
 
 namespace WorkWaveAPI.Controllers
 {
@@ -6,6 +10,72 @@ namespace WorkWaveAPI.Controllers
     [Route("api/v2/[controller]")]
     public class MembersController : Controller
     {
-      
+        UserManager<User> _userManager;
+        IRepository<Member> _membersRepository;
+
+        public MembersController(UserManager<User> userManager,IRepository<Member> memberRepository)
+        {
+            _userManager = userManager;
+            this._membersRepository= memberRepository;
+        }
+
+        [HttpGet("/getProjectMembers/{projectId}")]
+        public async Task<IActionResult> Get(int projectId)
+        {
+            if (projectId != 0)
+            {
+                var arr = _membersRepository.GetAll()
+                    .Where(x => x.ProjectId == projectId);
+                List<User> users = new List<User>();
+                foreach (var item in arr)
+                {
+                    users.Add(await _userManager.FindByIdAsync(item.UserId));
+                }
+                return Ok(users);
+            }
+            return BadRequest();
+
+        }
+
+        [HttpDelete("/removeProjectMember/{projectMember}")]
+        public async Task<IActionResult>RemoveProjectMember(int projectMember)
+        {
+            if(projectMember != 0)
+            {
+                var member=_membersRepository.GetById(projectMember);
+                if (member != null)
+                {
+                    _membersRepository.Remove(member);
+                    return Ok(member);
+                }
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPost("/addMember")]
+        public async Task<IActionResult> RemoveProjectMember([FromBody] AddMemberToProjectModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string userId;
+                if (string.IsNullOrEmpty(model.UserId))
+                {
+                    userId = (await _userManager.FindByNameAsync(model.UserName)).Id;
+                }
+                else userId= model.UserId;
+                Member member = new Member
+                {
+                    UserId = userId,
+                    ProjectId = model.ProjectId,
+                };
+                await _membersRepository.AddAsync(member);
+                return Ok(member);
+               
+            }
+            return BadRequest();
+        }
+
+
     }
 }
---
+++ WebApi/WorkWaveAPI/Managers/Base64Encoder.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkWaveAPI.Managers
+{
+    public class Base64Encoder
+    {
+        public static string GetBase64String(IFormFile file)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                byte[] bytes = new byte[stream.Length + 32];
+                int numBytesToRead = (int)stream.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = stream.Read(bytes, numBytesRead, 32);
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                string str = Convert.ToBase64String(bytes);
+                return $"data:image/{Path.GetExtension(file.FileName)};base64, {str}";
+            }
+        }
+    }
+}
